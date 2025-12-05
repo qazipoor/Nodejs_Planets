7a865825f9f72f903ba4350839560e40fc6c80f1
Ali Qazipoor <qazipoor@gmail.com>
2025-12-05 19:37:26 +0300

    Imported the csv file as readable file and used pipe(parser) to convert the file to JSON like type as well as writeable file and then narrowed down the search by filtering with some features of the planets. And finally added error handler as well as end event to print out the habitable planets.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const parser = parse(
+    {
+        comment: '#',
+        columns: true,
+        delimiter: ','
+    }
+);
+const csvFilePath = "./kepler_data.csv";
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' &&
+            planet['koi_insol'] > 0.36 && 
+            planet['koi_insol'] < 1.11 &&
+            planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream(csvFilePath)
+    .pipe(parser)
+    .on('data', (data)  => {
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
+    })
+    .on('error', (err) => {
+        console.error(`Error reading file: ${err.message}`);
+    })
+    .on('end', () => {
+        console.log(habitablePlanets);
+        console.log(`${habitablePlanets.length} habitable planets found.`);
+    })
+
+
+
+
